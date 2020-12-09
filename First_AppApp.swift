033e938ae17bd00907a88de97bd48724d7fc393e
CRS <crs@CRSs-iMac.local>
2020-12-09 01:25:44 +0100

    First commit

---
+++ First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by CRS on 8.12.20.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
